1a7e532d5e0b57d275b1ca9cb18d24a031e50bdf
Eritiel <Luna107@bk.ru>
2021-05-03 17:26:21 +0300

    Create 5.rb

---
+++ L2/5.rb
@@ -0,0 +1,26 @@
+def input n
+  puts "Введите элементы массива: "
+  array = Array.new(n){gets.to_i}
+  p array
+  array
+end
+
+def file
+  string = File.open('test.txt', 'r:UTF-8') {|file| file.read}
+  s = string.split(' ').map(&:to_i)
+  p s
+end
+
+def select n, path
+  case n
+  when 1
+    puts "Введите количество элементов массива: "
+    size = gets.to_i
+    input size
+  when 2
+    file
+  end
+end
+
+select 1,'test.txt'
+select 2,'test.txt'

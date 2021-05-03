52a97d0df448c9fef844549ccefa4cb72ed49d01
Eritiel <Luna107@bk.ru>
2021-05-03 18:47:17 +0300

    Update 6.rb

--- L2/6.rb
+++ L2/6.rb
@@ -1,14 +1,29 @@
 #1.9. Найти элементы, расположенные перед последним минимальным
-def find(arr)
-	result = Array.new
-	min = arr[0]
-	index = 0
-	for i in 1..arr.length-1
-		if arr[i] <= min
-			min = arr[i]
-			result += arr[index..i-1]
-			index = i
-		end
-	end
-	return result
+def find arr
+  result = Array.new
+  min = arr[0]
+  index = 0
+  for i in 1..arr.length-1
+    if arr[i] <= min
+      min = arr[i]
+      result += arr[index..i-1]
+      index = i
+    end
+  end
+  return result
+end
+
+#1.21. Найти элементы, расположенные после первого максимального
+def find arr
+  result = Array.new
+  max = arr[arr.length-1]
+  index = arr.length-1
+  for i in (1..arr.length-1).to_a.reverse
+    if arr[i] >= max
+      max = arr[i]
+      result.insert(0, arr[i+1..index])
+      index = i
+    end
+  end
+  return result
 end

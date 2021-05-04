a63b0cfb10846f55596480f18893ecd3847a86d2
Eritiel <Luna107@bk.ru>
2021-05-04 16:34:39 +0300

    Update 7.rb

--- L2/7.rb
+++ L2/7.rb
@@ -1,10 +1,26 @@
+#9
 def find arr
   min = array.rindex(array.min)
-  return arr[0..min-1]
+  p arr[0..min-1]
 end
 
+#21
 def find arr
   max = arr.index(arr.max)
-  return arr[max+1..arr.length-1]
+  p arr[max+1..arr.length-1]
 end
 
+#33
+def chech arr
+  p arr.all?{ |i, j| i * j < 0}
+end
+
+#45
+def sum arr, a, b
+ p arr.select{|elem| elem >= a and elem <= b}.sum
+end
+
+#57
+def find arr
+  p arr.filter_map.with_index{|elem, index| elem if index == 0 or elem > arr[0..index-1].sum}
+end

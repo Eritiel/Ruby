9dda8dc0d266e4939dd4cc17d4fa8add2afb0ddf
Eritiel <Luna107@bk.ru>
2021-05-04 16:48:59 +0300

    Update 6.rb

--- L2/6.rb
+++ L2/6.rb
@@ -1,5 +1,5 @@
 #1.9. Найти элементы, расположенные перед последним минимальным
-def find arr
+def find9 arr
   result = Array.new
   min = arr[0]
   index = 0
@@ -10,11 +10,11 @@ def find arr
       index = i
     end
   end
-  return result
+  p result
 end
 
 #1.21. Найти элементы, расположенные после первого максимального
-def find arr
+def find21 arr
   result = Array.new
   max = arr[arr.length-1]
   index = arr.length-1
@@ -25,11 +25,11 @@ def find arr
       index = i
     end
   end
-  return result
+  p result
 end
 
 #1.33. Проверка, чередуются ли в массиве положительные и отрицательные числа
-def chech arr
+def check33 arr
   for i in 0..arr.length-2
     if arr[i] * arr[i+1] > 0
       return false
@@ -39,18 +39,18 @@ def chech arr
 end
 
 #1.45. Найти сумму элементов, значение которых лежит в интервале a..b
-def sum arr, a, b
+def sum45 arr, a, b
   sum = 0
   for elem in arr
     if elem >= a and elem <= b
       sum += elem
     end
   end
-  return sum
+  p sum
 end
 
 #1.57. Найти элементы, большие, чем сумма всех предыдущих элементов
-def find arr
+def find57 arr
   result = Array.new
   sum = 0
   for elem in arr
@@ -59,5 +59,29 @@ def find arr
     end
     sum += elem
   end
-  return result
+  p result
+end
+
+puts "Введите количество элементов массива и сами элементы"
+arr = Array.new(gets.to_i){gets.to_i}
+answer = "1"
+while answer != "0"
+  puts "Введите номер задачи из списка:"
+  answer = STDIN.gets.chomp.strip
+  case answer
+  when "9"
+    find9 arr
+  when "21"
+    find21 arr
+  when "33"
+    p check33 arr
+  when "45"
+    print "Введите а = "
+    a = STDIN.gets.chomp.strip.to_i
+    print "Введите b = "
+    b = STDIN.gets.chomp.strip.to_i
+    sum45 arr, a, b
+  when "57"
+    find57 arr
+  end
 end

1fcbd3382dad3430ee336ce6ae3a4bca0fad6879
Eritiel <Luna107@bk.ru>
2021-06-25 19:17:34 +0300

    Update TourList.rb

--- L3/TourList.rb
+++ L3/TourList.rb
@@ -9,4 +9,25 @@ class TourList
     @list << tour
   end
   
+  def to_s
+    @list.each{|tour| tour.to_s}
+  end
+
+
+  def delete(tour)
+    @list.delete(tour)
+  end
+
+  def list_length
+    @list.length
+  end
+
+  def search(info)
+    @list.each{|tour| if [tour.name, tour.price, tour.country, tour.description].include? info
+                            return tour
+                       else return "Тура с искомыми значениями нет в списке"
+                       end
+    }
+  end
+  
 end

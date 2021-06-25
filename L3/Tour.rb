95522f14544476da2e43f7fc383b69f3dfe252d3
Eritiel <Luna107@bk.ru>
2021-06-25 16:52:06 +0300

    Update Tour.rb

--- L3/Tour.rb
+++ L3/Tour.rb
@@ -1,2 +1,12 @@
 class Tour
   attr_reader :id, :ticket_count, :name, :description, :price, :country, :image_link
+  
+  def initialize(id, ticket_count, name, description, price, country, image_link)
+    @id = id
+    self.ticket_count = ticket_count
+    self.name = name
+    self.description = description
+    self.price = price
+    self.country = country
+    self.image_link = image_link
+  end

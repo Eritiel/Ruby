17ce1a46fc3a0013f99105fd02dd7ab771121ef3
Eritiel <Luna107@bk.ru>
2021-07-02 20:21:11 +0300

    Update DB.rb

--- L3/DB.rb
+++ L3/DB.rb
@@ -1,1 +1,12 @@
 require 'mysql2'
+
+class DB
+  @@instance = nil
+  def initialize
+    @connection = Mysql2::Client.new(
+      :host => 'localhost',
+      :username => 'root',
+      :password => '******',
+      :database => 'tour')
+  end
+end

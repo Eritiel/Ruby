1f0bd88cdd0a4c7552ae3a4a826d292f7d013852
Eritiel <Luna107@bk.ru>
2021-07-02 21:14:53 +0300

    Update DB.rb

--- L3/DB.rb
+++ L3/DB.rb
@@ -17,4 +17,15 @@ class DB
     @@instance
   end
 
+  def read_list
+    results = @connection.query("SELECT * FROM tours")
+    list_tours = []
+    results.each do |row|
+        emp = Tour.new(row['id'], row['TicketsCount'], row['Name'], row['Desciption'],
+                           row['ImagePreview'], row['Price'], row['Country'])
+        list_tours << emp
+    end
+    list_tours
+  end
+
 end
